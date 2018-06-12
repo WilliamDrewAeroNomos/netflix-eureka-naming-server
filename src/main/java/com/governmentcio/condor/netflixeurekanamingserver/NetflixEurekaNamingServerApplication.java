b01920dd74a04dd4329b3f42046d1e77996713f0
William Drew <wdrew@governmentcio.com>
2018-06-12 11:21:35 -0400

    refactored package structure

--- src/main/java/com/governmentcio/condor/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/governmentcio/condor/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.netflixeurekanamingserver;
+package com.governmentcio.condor.netflixeurekanamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
